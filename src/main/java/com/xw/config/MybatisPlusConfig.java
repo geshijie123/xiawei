8e121d4b9dc62844fea3763716f2834b9a3f13c4
geshijie123 <1905548803@qq.com>
2018-11-28 16:59:21 +0800

    2

--- src/main/java/com/xw/config/MybatisPlusConfig.java
+++ src/main/java/com/xw/config/MybatisPlusConfig.java
@@ -9,7 +9,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 //Spring boot方式
 @EnableTransactionManagement
 @Configuration
-@MapperScan("com.xw.mapper.*")
 public class MybatisPlusConfig {
     /**
      * 分页插件
